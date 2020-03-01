da11178bdc87a6e9c0cfa84729708a0667c23e4a
Ravindu Fernando <ravindupf@gmail.com>
2020-03-01 18:52:01 +0530

    Mocha Configured

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const assert = require('assert');
+//Describe tests
+describe('Some Demo Tests', function(){
+    //Create Tests
+    it('adds two numbers togather', function(){
+        assert(2 + 3 === 5);
+    });
+});
+ 
